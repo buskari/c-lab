bf5a445faec1405c6e8b8a704f710ff053637095
Andris Buscariolli <andris.contact@gmail.com>
2020-05-18 20:41:40 -0300

    1. Learning about chars and strings (I/O)
    	1. Learning about chars and strings (I/O)

---
+++ charPlay.c
@@ -0,0 +1,26 @@
+/*
+ * Author: Andris Buscariolli
+ * Purpose: Char and string (I/O) practice
+ * Date: 18/02/2020
+*/
+
+#include <stdio.h>
+
+int main() 
+{
+    const int MAX_LEN = 100;
+
+    char ch;
+    char s[MAX_LEN];
+    char sen[MAX_LEN];
+
+    scanf("%c%*c", &ch);
+    scanf("%[^\n]%*c", s);
+    scanf("%[^\n]", sen);
+
+    printf("\n%c\n", ch);
+    printf("%s\n", s);
+    printf("%s", sen);
+    
+    return 0;
+}
