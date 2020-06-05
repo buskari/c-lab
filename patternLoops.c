b7287b73315f6dda9df3c78521b8ee8d5ca43cda
Andris Buscariolli <andris.contact@gmail.com>
2020-06-05 16:42:23 -0300

    1. solved
    	1. solved

--- patternLoops.c
+++ patternLoops.c
@@ -1,3 +1,9 @@
+/*
+ * Author: Andris Buscariolli
+ * Date: 06/05/2020
+ * Source: HackerRank.com
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
@@ -8,11 +14,27 @@ int main() {
     int n;
     scanf("%d", &n);
 
-    int value=n;
+    int end = (2*n-1); // this is the size of the matrix
   	
-    for(int i=1; i <= (2*n-1); i++) {
-        for(int j=1; j <= (2*n-1); j++) {
-            printf("%d ", value);
+    for(int i=1; i<=end; i++) {
+        for(int j=1; j<=end; j++) {
+            if(i+j<end+1){ //this takes every number above the foward diagonal
+                if(i<j)
+                    printf("%d ", n-(i-1));
+                else
+                    printf("%d ", n-(j-1));
+            } else if (i+j==end) { //this takes every number of the foward diagonal
+                if(i>j)
+                    printf("%d ", n-j);
+                else
+                    printf("%d ", n-i);
+            } else { // this takes everyone else
+                if(i>j)
+                    printf("%d ", n-(end-i));
+                else
+                    printf("%d ", n-(end-j));
+            }
+
         }
         printf("\n");
     }
