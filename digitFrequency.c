3d647242b50da59f494511f552abeb48af1aff2b
Andris Buscariolli <andris.contact@gmail.com>
2020-06-30 00:17:41 -0300

    digitFrequency refactored

--- digitFrequency.c
+++ digitFrequency.c
@@ -10,64 +10,21 @@
 
 int main() {
 
-    int count[10];
-    char *num;
+    char *num, c;
+    int count;
+
     num = malloc(1024 * sizeof(char));
     scanf("%[^\n]", num);
     num = realloc(num, strlen(num) + 1);
 
-    for(int i = 0; i < 10; i++)
-        count[i] = 0;
-
-    for(int i = 0; i < strlen(num); i++) {
-        switch (num[i]) {
-        case '1':
-            count[1]++;
-            break;
-
-        case '2':
-            count[2]++;
-            break;
-
-        case '3':
-            count[3]++;
-            break;
-
-        case '4':
-            count[4]++;
-            break;
-
-        case '5':
-            count[5]++;
-            break;
-
-        case '6':
-            count[6]++;
-            break;
-
-        case '7':
-            count[7]++;
-            break;
-
-        case '8':
-            count[8]++;
-            break;
-
-        case '9':
-            count[9]++;
-            break;
-
-        case '0':
-            count[0]++;
-            break;
-
-        default:
-            break;
+    for(int i = 0; i < 10; i++) {
+        count = 0;
+        for(int j = 0; j < strlen(num); j++) {
+            if(num[j] == i+48)
+                count++;
         }
+        printf("%d ", count);
     }
-
-    for(int i = 0; i < 10; i ++) 
-        printf("%d ", count[i]);
 
     return 0;
 }
